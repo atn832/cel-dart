a247b84debcd7adc19415f0796bc7fe15d564f53
Anh Tuan <atn832@gmail.com>
2023-01-05 10:24:52 +0900

    hide utility functions

--- lib/src/parser/parser.dart
+++ lib/src/parser/parser.dart
@@ -15,195 +15,192 @@ class Parser {
     final tree = parser.start();
     return visit(tree);
   }
+}
 
-  Expr visit(ParseTree tree) {
-    tree = unnest(tree);
-    if (tree is StartContext) {
-      return visitStart(tree);
-    }
-    if (tree is ExprContext) {
-      return visitExpr(tree);
-    }
-    if (tree is ConditionalOrContext) {
-      return visitConditionalOr(tree);
-    }
-    if (tree is ConditionalAndContext) {
-      return visitConditionalAnd(tree);
-    }
-    if (tree is RelationContext) {
-      return visitRelation(tree);
-    }
-    if (tree is CalcContext) {
-      return visitCalc(tree);
-    }
-    if (tree is UnaryContext) {
-      return visitUnary(tree);
-    }
-    if (tree is MemberCallContext) {
-      return visitMemberCall(tree);
-    }
-    if (tree is SelectContext) {
-      return visitSelect(tree);
-    }
-    if (tree is StringContext) {
-      return visitString(tree);
-    }
-    if (tree is IdentOrGlobalCallContext) {
-      return visitIdentOrGlobalCall(tree);
-    }
-    print('Unknown parse element ${tree.text}');
-    print(tree.runtimeType);
-    return Expr();
+Expr visit(ParseTree tree) {
+  tree = unnest(tree);
+  if (tree is StartContext) {
+    return visitStart(tree);
   }
-
-  Expr visitStart(StartContext c) {
-    return visit(c.expr()!);
+  if (tree is ExprContext) {
+    return visitExpr(tree);
   }
-
-  Expr visitExpr(ExprContext c) {
-    final result = visit(c.e!);
-    if (c.op == null) {
-      return result;
-    }
-    final ifTrue = visit(c.e1!);
-    final ifFalse = visit(c.e2!);
-    // TODO: convert op using the same mapping as https://github.com/google/cel-go/blob/master/common/operators/operators.go.
-    // TODO: support `e ? e1 : e2`. See https://github.com/google/cel-go/blob/442811f1e440a2052c68733a4dca0ab3e8898948/parser/gen/CEL.g4#L25
-    // and https://github.com/google/cel-go/blob/442811f1e440a2052c68733a4dca0ab3e8898948/parser/parser.go#L459.
-    return CallExpr()
-      ..function = c.op!.text!
-      ..args = [ifTrue, ifFalse];
+  if (tree is ConditionalOrContext) {
+    return visitConditionalOr(tree);
   }
-
-  Expr visitConditionalOr(ConditionalOrContext tree) {
-    // TODO: use a balancer like in https://github.com/google/cel-go/blob/442811f1e440a2052c68733a4dca0ab3e8898948/parser/parser.go#L463-L476.
-    return conditionalOrFromConditionalAndContexts(
-        '_||_', [tree.e!, ...tree.e1]);
+  if (tree is ConditionalAndContext) {
+    return visitConditionalAnd(tree);
   }
-
-  Expr conditionalOrFromConditionalAndContexts(
-      String function, List<ParserRuleContext> conditionalAndContexts) {
-    if (conditionalAndContexts.length == 1) {
-      return visit(conditionalAndContexts.first);
-    }
-    return CallExpr()
-      ..function = function
-      ..args = [
-        conditionalAndContexts.first,
-        conditionalOrFromConditionalAndContexts(
-            function, conditionalAndContexts.sublist(1))
-      ];
+  if (tree is RelationContext) {
+    return visitRelation(tree);
   }
-
-  Expr visitConditionalAnd(ConditionalAndContext tree) {
-    return conditionalOrFromConditionalAndContexts(
-        '_&&_', [tree.e!, ...tree.e1]);
+  if (tree is CalcContext) {
+    return visitCalc(tree);
   }
-
-  Expr visitCalc(CalcContext tree) {
-    // TODO: check against the map of operators. For example in becomes @in, not
-    // _in_. See
-    // https://github.com/google/cel-go/blob/442811f1e440a2052c68733a4dca0ab3e8898948/parser/parser.go#L515.
-    return CallExpr()
-      ..function = '_${tree.op!.text!}_'
-      ..args = [visit(tree.getChild(0)!), visit(tree.getChild(1)!)];
+  if (tree is UnaryContext) {
+    return visitUnary(tree);
   }
-
-  Expr visitUnary(UnaryContext tree) {
-    return StringLiteralExpr()..value = '<<error>>';
+  if (tree is MemberCallContext) {
+    return visitMemberCall(tree);
   }
-
-  Expr visitMemberCall(MemberCallContext tree) {
-    final operand = visit(tree.member()!);
-    final id = tree.id!.text!;
-    // TODO: I'm assuming it is a Select. Not parsing Member properly. See
-    // https://github.com/google/cel-go/blob/442811f1e440a2052c68733a4dca0ab3e8898948/parser/parser.go#L387-L396.
-    return SelectExpr()
-      ..field = id
-      ..operand = operand;
+  if (tree is SelectContext) {
+    return visitSelect(tree);
   }
-
-  Expr visitSelect(SelectContext tree) {
-    final operand = visit(tree.member()!);
-    final id = tree.id!.text!;
-    return SelectExpr()
-      ..field = id
-      ..operand = operand;
+  if (tree is StringContext) {
+    return visitString(tree);
   }
-
-  Expr visitRelation(RelationContext tree) {
-    // TODO: convert to the Operator name properly.
-    final op = tree.op!.text!;
-    return CallExpr()
-      ..function = op
-      ..args = [visit(tree.relation(0)!), visit(tree.relation(1)!)];
+  if (tree is IdentOrGlobalCallContext) {
+    return visitIdentOrGlobalCall(tree);
   }
+  print('Unknown parse element ${tree.text}');
+  print(tree.runtimeType);
+  return Expr();
+}
 
-  Expr visitString(StringContext tree) {
-    return StringLiteralExpr()
-      // Unquote.
-      // TODO: implement unescape in the unquote. See
-      // https://github.com/google/cel-go/blob/442811f1e440a2052c68733a4dca0ab3e8898948/parser/parser.go#L843.
-      ..value = tree.text.substring(1, tree.text.length - 1);
+Expr visitStart(StartContext c) {
+  return visit(c.expr()!);
+}
+
+Expr visitExpr(ExprContext c) {
+  final result = visit(c.e!);
+  if (c.op == null) {
+    return result;
   }
+  final ifTrue = visit(c.e1!);
+  final ifFalse = visit(c.e2!);
+  // TODO: convert op using the same mapping as https://github.com/google/cel-go/blob/master/common/operators/operators.go.
+  // TODO: support `e ? e1 : e2`. See https://github.com/google/cel-go/blob/442811f1e440a2052c68733a4dca0ab3e8898948/parser/gen/CEL.g4#L25
+  // and https://github.com/google/cel-go/blob/442811f1e440a2052c68733a4dca0ab3e8898948/parser/parser.go#L459.
+  return CallExpr()
+    ..function = c.op!.text!
+    ..args = [ifTrue, ifFalse];
+}
 
-  // Based on
-  // https://github.com/google/cel-go/blob/442811f1e440a2052c68733a4dca0ab3e8898948/parser/parser.go#L950.
-  // It unwraps the "concrete" ParseTree. For example, a Member rule is wrapped
-  // by an empty Unary, which is wrapped by an empty Relation (with no
-  // operator).
-  ParseTree unnest(ParseTree t) {
-    if (t is ExprContext) {
-      return t.op != null
-          ? t // conditionalOr op='?' conditionalOr : expr
-          : unnest(t.e!); // conditionalOr
-    }
-    if (t is ConditionalOrContext) {
-      return t.ops.isNotEmpty
-          ? t // conditionalAnd (ops=|| conditionalAnd)*
-          : unnest(t.e!); // conditionalAnd
-    }
-    if (t is ConditionalAndContext) {
-      return t.ops.isNotEmpty
-          ? t // relation (ops=&& relation)*
-          : unnest(t.e!); // relation
-    }
-    if (t is RelationContext) {
-      return t.op != null
-          ? t // relation op relation
-          : unnest(t.calc()!); // calc
-    }
-    if (t is CalcContext) {
-      return t.op != null
-          ? t // calc op calc
-          : unnest(t.unary()!); // unary
-    }
-    if (t is MemberExprContext) {
-      return unnest(t.member()!);
-    }
-    if (t is PrimaryExprContext) {
-      return unnest(t.primary()!);
-    }
-    if (t is NestedContext) {
-      return unnest(t.e!);
-    }
-    if (t is ConstantLiteralContext) {
-      return unnest(t.literal()!);
-    }
-    // TODO: port the remaining types.
-    return t;
+Expr visitConditionalOr(ConditionalOrContext tree) {
+  // TODO: use a balancer like in https://github.com/google/cel-go/blob/442811f1e440a2052c68733a4dca0ab3e8898948/parser/parser.go#L463-L476.
+  return conditionalOrFromConditionalAndContexts('_||_', [tree.e!, ...tree.e1]);
+}
+
+Expr conditionalOrFromConditionalAndContexts(
+    String function, List<ParserRuleContext> conditionalAndContexts) {
+  if (conditionalAndContexts.length == 1) {
+    return visit(conditionalAndContexts.first);
   }
+  return CallExpr()
+    ..function = function
+    ..args = [
+      conditionalAndContexts.first,
+      conditionalOrFromConditionalAndContexts(
+          function, conditionalAndContexts.sublist(1))
+    ];
+}
 
-  // Based on
-  // https://github.com/google/cel-go/blob/442811f1e440a2052c68733a4dca0ab3e8898948/parser/parser.go#L652.
-  Expr visitIdentOrGlobalCall(IdentOrGlobalCallContext tree) {
-    if (tree.op != null) {
-      // TODO: Handle global call or macro.
-      throw Exception();
-    }
-    // TODO: Check for reserved identifiers and throw errors.
+Expr visitConditionalAnd(ConditionalAndContext tree) {
+  return conditionalOrFromConditionalAndContexts('_&&_', [tree.e!, ...tree.e1]);
+}
 
-    return IdentExpr()
-      ..name = '${tree.leadingDot?.text ?? ''}${tree.id!.text!}';
+Expr visitCalc(CalcContext tree) {
+  // TODO: check against the map of operators. For example in becomes @in, not
+  // _in_. See
+  // https://github.com/google/cel-go/blob/442811f1e440a2052c68733a4dca0ab3e8898948/parser/parser.go#L515.
+  return CallExpr()
+    ..function = '_${tree.op!.text!}_'
+    ..args = [visit(tree.getChild(0)!), visit(tree.getChild(1)!)];
+}
+
+Expr visitUnary(UnaryContext tree) {
+  return StringLiteralExpr()..value = '<<error>>';
+}
+
+Expr visitMemberCall(MemberCallContext tree) {
+  final operand = visit(tree.member()!);
+  final id = tree.id!.text!;
+  // TODO: I'm assuming it is a Select. Not parsing Member properly. See
+  // https://github.com/google/cel-go/blob/442811f1e440a2052c68733a4dca0ab3e8898948/parser/parser.go#L387-L396.
+  return SelectExpr()
+    ..field = id
+    ..operand = operand;
+}
+
+Expr visitSelect(SelectContext tree) {
+  final operand = visit(tree.member()!);
+  final id = tree.id!.text!;
+  return SelectExpr()
+    ..field = id
+    ..operand = operand;
+}
+
+Expr visitRelation(RelationContext tree) {
+  // TODO: convert to the Operator name properly.
+  final op = tree.op!.text!;
+  return CallExpr()
+    ..function = op
+    ..args = [visit(tree.relation(0)!), visit(tree.relation(1)!)];
+}
+
+Expr visitString(StringContext tree) {
+  return StringLiteralExpr()
+    // Unquote.
+    // TODO: implement unescape in the unquote. See
+    // https://github.com/google/cel-go/blob/442811f1e440a2052c68733a4dca0ab3e8898948/parser/parser.go#L843.
+    ..value = tree.text.substring(1, tree.text.length - 1);
+}
+
+// Based on
+// https://github.com/google/cel-go/blob/442811f1e440a2052c68733a4dca0ab3e8898948/parser/parser.go#L950.
+// It unwraps the "concrete" ParseTree. For example, a Member rule is wrapped
+// by an empty Unary, which is wrapped by an empty Relation (with no
+// operator).
+ParseTree unnest(ParseTree t) {
+  if (t is ExprContext) {
+    return t.op != null
+        ? t // conditionalOr op='?' conditionalOr : expr
+        : unnest(t.e!); // conditionalOr
   }
+  if (t is ConditionalOrContext) {
+    return t.ops.isNotEmpty
+        ? t // conditionalAnd (ops=|| conditionalAnd)*
+        : unnest(t.e!); // conditionalAnd
+  }
+  if (t is ConditionalAndContext) {
+    return t.ops.isNotEmpty
+        ? t // relation (ops=&& relation)*
+        : unnest(t.e!); // relation
+  }
+  if (t is RelationContext) {
+    return t.op != null
+        ? t // relation op relation
+        : unnest(t.calc()!); // calc
+  }
+  if (t is CalcContext) {
+    return t.op != null
+        ? t // calc op calc
+        : unnest(t.unary()!); // unary
+  }
+  if (t is MemberExprContext) {
+    return unnest(t.member()!);
+  }
+  if (t is PrimaryExprContext) {
+    return unnest(t.primary()!);
+  }
+  if (t is NestedContext) {
+    return unnest(t.e!);
+  }
+  if (t is ConstantLiteralContext) {
+    return unnest(t.literal()!);
+  }
+  // TODO: port the remaining types.
+  return t;
+}
+
+// Based on
+// https://github.com/google/cel-go/blob/442811f1e440a2052c68733a4dca0ab3e8898948/parser/parser.go#L652.
+Expr visitIdentOrGlobalCall(IdentOrGlobalCallContext tree) {
+  if (tree.op != null) {
+    // TODO: Handle global call or macro.
+    throw Exception();
+  }
+  // TODO: Check for reserved identifiers and throw errors.
+
+  return IdentExpr()..name = '${tree.leadingDot?.text ?? ''}${tree.id!.text!}';
 }
