8590a2c7031de5b1571db3124cb3b6222c239b7a
Anh Tuan <atn832@gmail.com>
2023-01-07 12:38:41 +0900

    hide Program.interpreter from dart docs

--- lib/src/cel/program.dart
+++ lib/src/cel/program.dart
@@ -13,13 +13,14 @@ import 'options.dart';
 class Program {
   // Port of
   // https://github.com/google/cel-go/blob/442811f1e440a2052c68733a4dca0ab3e8898948/cel/program.go#L150.
-  Program(this.environment, this.ast, [this.options = const []]) {
+  Program(this.environment, this.ast,
+      [List<ProgramOption> options = const []]) {
     // Build the dispatcher, interpreter, and default program value.
     dispatcher = Dispatcher([]);
 
     // https://github.com/google/cel-go/blob/442811f1e440a2052c68733a4dca0ab3e8898948/cel/program.go#L183-L190
     final attributeFactory = AttributeFactory();
-    interpreter = Interpreter(
+    _interpreter = Interpreter(
         attributeFactory: attributeFactory,
         dispatcher: dispatcher,
         adapter: environment.adapter);
@@ -40,23 +41,22 @@ class Program {
 
   final Environment environment;
   final Ast ast;
-  final List<ProgramOption> options;
 
   late Dispatcher dispatcher;
-  late Interpreter interpreter;
-  late Interpretable interpretable;
+  late Interpreter _interpreter;
+  late Interpretable _interpretable;
 
   // Shortcut: cel-go returns a Val.
   dynamic evaluate(Map<String, dynamic> input) {
     // Skipped porting Pools since there is no concurrency in this
     // implementation.
     final Activation vars = EvalActivation(input);
-    return interpretable.evaluate(vars).convertToNative();
+    return _interpretable.evaluate(vars).convertToNative();
   }
 
   void _initInterpretable() {
     final checkedExpression = _astToCheckedExpr(ast);
-    interpretable = interpreter.interpet(checkedExpression);
+    _interpretable = _interpreter.interpet(checkedExpression);
   }
 }
 
