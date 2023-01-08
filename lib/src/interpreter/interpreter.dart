f4d63761f4d090a6e4a0510826722ca90a6191e8
Anh Tuan <atn832@gmail.com>
2023-01-08 15:15:35 +0900

    remove ununsed interpreter.Standard

--- lib/src/interpreter/interpreter.dart
+++ lib/src/interpreter/interpreter.dart
@@ -3,7 +3,6 @@ import 'package:cel/src/common/types/ref/provider.dart';
 import '../cel/checked_expression.dart';
 import 'attribute_factory.dart';
 import 'dispatcher.dart';
-import 'functions/standard.dart';
 import 'interpretable.dart';
 import 'planner.dart';
 
@@ -12,18 +11,6 @@ class Interpreter {
       {required this.attributeFactory,
       required this.dispatcher,
       required this.adapter});
-
-  /// NewStandardInterpreter builds a Dispatcher and TypeProvider with support for all of the CEL
-  /// builtins defined in the language definition.
-  /// https://github.com/google/cel-go/blob/377a0bba20d07926e0583b4e604509ca7f3583b7/interpreter/interpreter.go#L179
-  factory Interpreter.standard(
-      {required AttributeFactory attributeFactory,
-      required TypeAdapter adapter}) {
-    return Interpreter(
-        attributeFactory: attributeFactory,
-        adapter: adapter,
-        dispatcher: Dispatcher(standardOverloads()));
-  }
 
   final AttributeFactory attributeFactory;
   final Dispatcher dispatcher;
